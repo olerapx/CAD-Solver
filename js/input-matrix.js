e79782f1c88d428bd6647d487d8c003145b1680a
olerapx <olerapx0@gmail.com>
2016-10-10 23:44:04 +0300

    Initial commit

---
+++ js/input-matrix.js
@@ -0,0 +1,5 @@
+$.fn.addTable = function () {
+	
+}
+
+$("div.input-matrix").addTable();
---
+++ js/page-loader.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    loadPage("html/main.html");
+});
+
+/* Sets handlers for navbar */
+$("ul.navbar-nav li:not(.nav-algos)").each(function() {
+
+    $(this).on("click", function() {    	
+    	loadPage(($(this)).attr("data-page"));
+
+        $("ul.navbar-nav li").toggleClass("active", false);
+        $(this).toggleClass("active", true);
+    });
+});
+
+function loadPage(page) {
+	$(".container").empty();
+	$(".container").load(page, function() {
+		$('body').css('background-image', 'url('+($('.page-bg')).attr("src")+')');
+	});
+}
