6ed98747c7d2f4401700cf30a18de00e3c1cb305
Blendeee <bl.man.ten@gmail.com>
2016-10-24 22:27:21 +0300

    2dsp offline

---
+++ assets/js/pack-table-handler.js
@@ -0,0 +1,10 @@
+let table = new PackTable("div.pack-table");
+table.extendTableWithDefaultName(0);
+table.extendTableWithDefaultName(1);
+
+$("#btn-solve").on('click', function () {
+
+	$('#div-result').removeClass("hidden");
+	$('#el3').collapse("show");
+	$('#result').text(table.toJSON());
+});
